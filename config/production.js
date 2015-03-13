f1b6b6151ce4d38a4c839ef50b2bd848ab4655ce
Dave Kerr <dave_kerr@mckinsey.com>
2015-03-13 21:48:02 +0100

    fixed mongo ip

--- config/production.js
+++ config/production.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "Production",
   port: 8081,         // nginx running as a reverse proxy, 80 -> 8081 
   db: {
-    connectionString: 'mongodb://localhost:27017/onedown'
+    connectionString: 'mongodb://52.10.235.154:27017/onedown'
   },
   auth0: {
     secret: process.env.ONEDOWN_AUTH0_SECRET,
